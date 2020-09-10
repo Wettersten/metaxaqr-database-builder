4061e6eef8d3d8da03edf2cc144e2eef263af6d4
Wettersten <59127164+Wettersten@users.noreply.github.com>
2020-09-10 14:01:29 +0200

    Update meta_clustering.py

--- meta_clustering.py
+++ meta_clustering.py
@@ -1,7 +1,7 @@
 import argparse
 from src.meta_clustering.parsing import create_parser, return_args
-from src.meta_clustering.parsing import id_range_to_list
-from src.meta_clustering.parsing import float_to_str_id
+from src.meta_clustering.handling import id_range_to_list
+from src.meta_clustering.handling import float_to_str_id
 from src.meta_clustering.handling import create_dir_structure
 from src.meta_clustering.clustering import cluster_vs
 from src.meta_clustering.cluster_tax import create_cluster_tax
