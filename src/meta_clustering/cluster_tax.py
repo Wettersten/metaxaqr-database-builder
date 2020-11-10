82a19ba4e6af02211059a9acf030ee403fa85fcc
Wettersten <59127164+Wettersten@users.noreply.github.com>
2020-11-10 17:25:25 +0100

    find_spsplits from most to least words in species fix

--- src/meta_clustering/cluster_tax.py
+++ src/meta_clustering/cluster_tax.py
@@ -443,18 +443,15 @@ def chlor_mito_flag(cluster):
 
 
 def find_spsplits(tax_cluster):
-    """Gets how many words (split by spaces) in the entry with most words in
+    """Gets how many words (split by spaces) in the entry with least words in
     the tax_cluster.
     """
     sps = []
-    sp_splits = 0
 
     for tax in tax_cluster:
-        sps.append(tax[-1].split(" "))
+        sps.append(len(tax[-1].split(" ")))
 
-    for sp in sps:
-        if len(sp) > sp_splits:
-            sp_splits = len(sp)
+    sp_splits = min(sps)
 
     return sp_splits
 
